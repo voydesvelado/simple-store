8549bcd859d70625d872f9a1a152298219dbc6af
novacane <mudakmudaka@gmail.com>
2020-09-23 18:52:19 -0500

    Updates

---
+++ precache-manifest.b5e24ae09e3b4e7fc6f1fd6f447796b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa972872dc910f28f22f685379c8cda3",
+    "url": "/simple-store/index.html"
+  },
+  {
+    "revision": "c1455679cde437f9bdc3",
+    "url": "/simple-store/static/css/main.b557f997.chunk.css"
+  },
+  {
+    "revision": "5345593fbbc1d14d499a",
+    "url": "/simple-store/static/js/2.731dda9f.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/simple-store/static/js/2.731dda9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1455679cde437f9bdc3",
+    "url": "/simple-store/static/js/main.d64ca818.chunk.js"
+  },
+  {
+    "revision": "4efcbc76802bd05c61f0",
+    "url": "/simple-store/static/js/runtime-main.3a6faad9.js"
+  },
+  {
+    "revision": "bf03f45cdf9c49336ee60698e2ed9be0",
+    "url": "/simple-store/static/media/supermarket.bf03f45c.svg"
+  }
+]);
